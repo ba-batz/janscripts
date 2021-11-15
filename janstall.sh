808986675f66cbbd38c2de37abb4e5fb99a59868
liquid <87938567+liqu1ds@users.noreply.github.com>
2021-11-15 21:55:42 +0100

    Add files via upload

--- janstall.sh
+++ janstall.sh
@@ -18,8 +18,10 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Add user(s)
+echo "root password: "
 passwd
 
+echo "user password: "
 useradd -m -G games,wheel,video,dbus "$NAMEUSER"
 passwd "$NAMEUSER"
 
@@ -32,6 +34,9 @@ sed -i "s/myhostname/$NAMEHOST/g" /etc/hosts
 
 pacman -Syu --noconfirm networkmanager networkmanager-s6
 s6-rc-bundle-update -c /etc/s6/rc/compiled add default NetworkManager
+
+# Xorg
+pacman -S --noconfirm xorg
 
 # Add Repositories
 pacman -S --noconfirm artix-archlinux-support
@@ -48,11 +53,10 @@ mv ./makepkg.conf /etc/makepkg.conf
 pacman -Syu --noconfirm xfce4 xfce4-goodies
 
 # Other startup Services
-pacman -S --noconfirm tlp tlp-s6 lightdm lightdm-s6 elogind eloging-s6 backlight-s6
-s6-rc-bundle-update -c /etc/s6/compiled add default tlp
-s6-rc-bundle-update -c /etc/s6/compiled add default lightdm
-s6-rc-bundle-update -c /etc/s6/compiled add default elogind
-s6-rc-bundle-update -c /etc/s6/compiled add default backlight
+pacman -S --noconfirm tlp tlp-s6 lightdm lightdm-s6 backlight-s6
+s6-rc-bundle-update add default tlp
+s6-rc-bundle-update add default lightdm
+s6-rc-bundle-update add default backlight
 
 # Replace sudo with doas
 pacman -Rns --noconfirm sudo
@@ -61,35 +65,39 @@ ln -sfT doas /usr/local/bin/sudo
 mv ./doas.conf /etc/doas.conf
 
 # Themeing Desktop
-pacman -S --noconfirm x-arc-darker papirus-icon-theme
+pacman -S --noconfirm  papirus-icon-theme
 #pacman -S --noconfirm lightdm-gtk-greeter
 
 
 # Graphics
-#pacman -S --noconfirm xf86-video-intel mesa libva libva-intel-driver vulkan-intel
-#mv ./20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
+pacman -S --noconfirm xf86-video-intel mesa libva libva-intel-driver vulkan-intel
+mv ./20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
 
-pacman -S --noconfirm xf86-video-amdgpu mesa libva libva-mesa-driver amdvlk
-mv ./20-amdgpu.conf /etc/X11/xorg.conf.d/20-amdgpu.conf
+#pacman -S --noconfirm xf86-video-amdgpu mesa libva libva-mesa-driver amdvlk
+#mv ./20-amdgpu.conf /etc/X11/xorg.conf.d/20-amdgpu.conf
 
 # dash instead of bash
 pacman -S --noconfirm dash dashbinsh
 ln -sfT dash /usr/bin/sh
 
 # Update microcode
-#pacman -S --noconfirm intel-ucode
-pacman -S --noconfirm amd-ucode
+pacman -S --noconfirm intel-ucode
+#pacman -S --noconfirm amd-ucode
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Setup touchpad
+mv ./30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 
 # Make updates automatic
 echo "pacman -Scc --noconfirm && pacman -Syu --noconfirm &" >> /etc/xprofile
 
 # Fonts
-pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttc-iosevka ttf-dejavu ttf-ms-fonts ttf-nerd-fonts-symbols ttf-roboto-mono otf-fira-sans otf-hasklig
+pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttc-iosevka ttf-dejavu ttf-ms-fonts ttf-nerd-fonts-symbols ttf-roboto-mono otf-fira-sans ttf-ms-fonts
 
 
 # Minecraft
 pacman -S --noconfirm gamemode gnome-keyring orca minecraft-launcher
+
 
 # Prepare for manual instalation
 mv ./xfce.sh /home/$NAMEUSER/ 
