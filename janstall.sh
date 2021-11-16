a51ebca7b9626df667d37d4db8bec82ab7b7b3ad
liquid <87938567+liqu1ds@users.noreply.github.com>
2021-11-16 18:11:26 +0100

    Add files via upload

--- janstall.sh
+++ janstall.sh
@@ -66,7 +66,7 @@ mv ./doas.conf /etc/doas.conf
 
 # Themeing Desktop
 pacman -S --noconfirm  papirus-icon-theme
-#pacman -S --noconfirm lightdm-gtk-greeter
+pacman -S --noconfirm lightdm-gtk-greeter
 
 
 # Graphics
@@ -100,4 +100,11 @@ pacman -S --noconfirm gamemode gnome-keyring orca minecraft-launcher
 
 
 # Prepare for manual instalation
-mv ./xfce.sh /home/$NAMEUSER/ 
+mv ./xfce.sh /home/$NAMEUSER/
+
+# Steam Proton and Wine
+pacman -S --needed --noconfirm lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
+pacman -Sy --noconfirm
+pacman -S --noconfirm wine-staging winetricks
+pacman -S --noconfirm giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox
+pacman -Syu --noconfirm steam steam-native-runtime proton-ge-custom
